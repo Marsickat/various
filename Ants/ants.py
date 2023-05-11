65727f92fdbcb193ec490ef054bb08b30bf4f10f
Marsickat <105936508+Marsickat@users.noreply.github.com>
2023-05-11 08:01:05 +0300

    Update ants.py
    Добавление констант

--- Ants/ants.py
+++ Ants/ants.py
@@ -1,5 +1,8 @@
 import json
 
+MODE = "pve"  # "pve" or "pvp"
+TYPE = "Shooter"  # "Guardian" or "Shooter" or "Carrier"
+
 with open("ants.json", encoding="utf-8") as f:
     data = json.load(f)
     lst_Splus, lst_S, lst_Aplus, lst_A, lst_Bplus, lst_B, lst_Cplus, lst_C, lst_Dplus, lst_D, lst_F = [], [], [], [], [], [], [], [], [], [], []
@@ -7,27 +10,27 @@ with open("ants.json", encoding="utf-8") as f:
         for name, chars in ant.items():
             for c in chars:
                 if c["available"]:
-                    if c["type"] == "Strelok" or c["type"] == "Universal":
-                        if c["pve"] == "S+":
+                    if c["type"] == TYPE or c["type"] == "Universal":
+                        if c[MODE] == "S+":
                             lst_Splus.append(f"{c['translate']}, {c['skill_1']}-{c['skill_2']}-{c['skill_3']}")
-                        elif c["pve"] == "S":
+                        elif c[MODE] == "S":
                             lst_S.append(f"{c['translate']}, {c['skill_1']}-{c['skill_2']}-{c['skill_3']}")
-                        elif c["pve"] == "A+":
+                        elif c[MODE] == "A+":
                             lst_Aplus.append(f"{c['translate']}, {c['skill_1']}-{c['skill_2']}-{c['skill_3']}")
-                        elif c["pve"] == "A":
+                        elif c[MODE] == "A":
                             lst_A.append(f"{c['translate']}, {c['skill_1']}-{c['skill_2']}-{c['skill_3']}")
-                        elif c["pve"] == "B+":
+                        elif c[MODE] == "B+":
                             lst_Bplus.append(f"{c['translate']}, {c['skill_1']}-{c['skill_2']}-{c['skill_3']}")
-                        elif c["pve"] == "B":
+                        elif c[MODE] == "B":
                             lst_B.append(f"{c['translate']}, {c['skill_1']}-{c['skill_2']}-{c['skill_3']}")
-                        elif c["pve"] == "C+":
+                        elif c[MODE] == "C+":
                             lst_Cplus.append(f"{c['translate']}, {c['skill_1']}-{c['skill_2']}-{c['skill_3']}")
-                        elif c["pve"] == "C":
+                        elif c[MODE] == "C":
                             lst_C.append(f"{c['translate']}, {c['skill_1']}-{c['skill_2']}-{c['skill_3']}")
-                        elif c["pve"] == "D+":
+                        elif c[MODE] == "D+":
                             lst_Dplus.append(f"{c['translate']}, {c['skill_1']}-{c['skill_2']}-{c['skill_3']}")
-                        elif c["pve"] == "D":
+                        elif c[MODE] == "D":
                             lst_D.append(f"{c['translate']}, {c['skill_1']}-{c['skill_2']}-{c['skill_3']}")
-                        elif c["pve"] == "F":
+                        elif c[MODE] == "F":
                             lst_F.append(f"{c['translate']}, {c['skill_1']}-{c['skill_2']}-{c['skill_3']}")
 print(lst_Splus, lst_S, lst_Aplus, lst_A, lst_Bplus, lst_B, lst_Cplus, lst_C, lst_Dplus, lst_D, lst_F, sep="\n")
